dba1e29f61f524cca83a739e1d87c8978ee2e18a
Andreu Lubashenko <nob22825@mail.ru>
2024-06-24 00:06:21 +0300

    Delete 353502/Любашенко А.С./LabWork_2 directory

--- 353502/Любашенко А.С./LabWork_2/Task_2/fileholder.cpp
+++
@@ -1,8 +0,0 @@
-#include "fileholder.h"
-
-FileHolder::FileHolder() {}
-
-QFile FileHolder::PriceList()
-{
-    return QFile("/home/denis/Qtprojects/LABA2/Task2/build-Orders-Desktop-Debug/PRICELIST.txt");
-}
--- 353502/Любашенко А.С./LabWork_2/Task_2/pricwlistwork.cpp
+++
@@ -1,3 +0,0 @@
-#include "pricwlistwork.h"
-
-PricwListWork::PricwListWork() {}
